0cce9a8aa32e745c1535198d01b8d3ff8e01789c
dc <dc@wooduan.com>
2016-10-09 23:28:50 +0800

    写了一些TCPSocket的东西

--- Assets/ResetCore/NetPost/Socket/TcpSocket.cs
+++ Assets/ResetCore/NetPost/Socket/TcpSocket.cs
@@ -1,173 +1,333 @@
-﻿//using UnityEngine;
-//using System.Collections;
-//using System.Net.Sockets;
-//using System;
-//using System.Net;
-//using ResetCore.Event;
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System;
+using System.Net;
+using ResetCore.Event;
 
-//namespace ResetCore.NetPost
-//{
-//    public class TcpSocket
-//    {
+namespace ResetCore.NetPost
+{
+    public class TcpSocket
+    {
+        /// <summary>
+        ///远端端口
+        /// </summary>
+        public int remotePort { get; private set; }
+        /// <summary>
+        /// 远端地址
+        /// </summary>
+        public string remoteAddress { get; private set; }
+        /// <summary>
+        /// 本地端口
+        /// </summary>
+        public int localPort { get; private set; }
+        /// <summary>
+        /// 本地地址
+        /// </summary>
+        public string localAddress { get; private set; }
+        /// <summary>
+        /// Socket变量
+        /// </summary>
+        private Socket socket;
 
-//        public int remotePort { get; private set; }
+        /// <summary>
+        /// 是否为主动关闭
+        /// </summary>
+        private bool isCloseSelf = false;
 
-//        public string remoteAddress { get; private set; }
+        /// <summary>
+        /// 接收数据缓冲区
+        /// </summary>
+        private byte[] receiveBuffer = new byte[1024 * 5];
 
-//        public int localPort { get; private set; }
+        /// <summary>
+        /// 是否连接
+        /// </summary>
+        public bool isConneted
+        {
+            get
+            {
+                if (socket == null || !socket.Connected)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
 
-//        public string localAddress { get; private set; }
+        #region 公开的回调函数
 
-//        private Socket socket;
+        public TcpSocketListenDelegate onListen { get; private set; }
+        public TcpSocketConnectDelegate onConnect { get; private set; }
+        public TcpSocketDisconnectDelegate onDisconnect { get; private set; }
+        public TcpSocketErrorDelegate onError { get; private set; }
+        public TcpSocketReceiveDelegate onReveive { get; private set; }
+        public TcpSocketSendDelegate onSend { get; private set; }
 
-//        /// <summary>
-//        /// 是否连接
-//        /// </summary>
-//        public bool isConneted
-//        {
-//            get
-//            {
-//                if (socket == null)
-//                {
-//                    return false;
-//                }
-//                else if (!socket.Connected)
-//                {
-//                    return false;
-//                }
-//                else
-//                {
-//                    return true;
-//                }
-//            }
-//        }
+        #endregion
 
-//        private AsyncCallback acceptCallback;
-//        private AsyncCallback connectCallback;
-//        private AsyncCallback disconnectCallback;
-//        private AsyncCallback sendCallback;
-//        private AsyncCallback receiveCallback;
+        public TcpSocket(Socket socket = null)
+        {
+            acceptCallback = new AsyncCallback(OnAccept);
+            connectCallback = new AsyncCallback(OnConnect);
+            disconnectCallback = new AsyncCallback(OnDisconnect);
+            sendCallback = new AsyncCallback(OnSend);
+            receiveCallback = new AsyncCallback(OnReceive);
 
-//        public TcpSocketListenDelegate onListen { get; private set; }
-//        public TcpSocketConnectDelegate onConnect { get; private set; }
-//        public TcpSocketDisconnectDelegate onDisconnect { get; private set; }
-//        public TcpSocketErrorDelegate onError { get; private set; }
-//        public TcpSocketReceiveDelegate onReveive { get; private set; }
-//        public TcpSocketSendDelegate onSend { get; private set; }
+            if (socket != null)
+            {
+                this.socket = socket;
 
+                //远端地址
+                IPEndPoint remote = socket.RemoteEndPoint as IPEndPoint;
+                remoteAddress = remote.Address.ToString();
+                remotePort = remote.Port;
 
-//        public TcpSocket(Socket socket = null)
-//        {
-//            acceptCallback = new AsyncCallback(OnAccept);
-//            connectCallback = new AsyncCallback(OnConnect);
-//            disconnectCallback = new AsyncCallback(OnDisconnect);
-//            sendCallback = new AsyncCallback(OnSend);
-//            receiveCallback = new AsyncCallback(OnReceive);
-
-//            if(socket != null)
-//            {
-//                this.socket = socket;
-
-//                //远端地址
-//                IPEndPoint remote = socket.RemoteEndPoint as IPEndPoint;
-//                remoteAddress = remote.Address.ToString();
-//                remotePort = remote.Port;
-
-//                //本地地址
-//                IPEndPoint local = socket.LocalEndPoint as IPEndPoint;
-//                localAddress = local.Address.ToString();
-//                localPort = local.Port;
-//            }
-//        }
-
-
-//        #region 公开方法
-//        /// <summary>
-//        /// 开始监听
-//        /// </summary>
-//        public bool BeginListen(int port, int queueSize = 1000)
-//        {
-//            localAddress = "127.0.0.1";
-//            localPort = port;
-
-//            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-//            try
-//            {
-//                socket.Bind(new IPEndPoint(IPAddress.Any, localPort));
-//                socket.Listen(queueSize);
-//                socket.BeginAccept(acceptCallback, null);
-//            }
-//            catch (SocketException se)
-//            {
-//                if (onListen != null)
-//                    onListen(2, se.ErrorCode, se.Message);
-
-//                if (onError != null)
-//                    onError(SocketState.BEGIN_LISTEN, se.ErrorCode, se.Message);
-//            }
-//            catch(Exception exp)
-//            {
-//                if (onListen != null)
-//                    onListen(2, 0, exp.Message);
-
-//                if (onError != null)
-//                    onError(2, 0, exp.Message);
-//            }
-//        }
-
-//        /// <summary>
-//        /// 停止监听
-//        /// </summary>
-//        public void StopListen()
-//        {
-
-//        }
-
-//        /// <summary>
-//        /// 连接
-//        /// </summary>
-//        public void Connect()
-//        {
-
-//        }
-
-//        /// <summary>
-//        /// 断开连接
-//        /// </summary>
-//        public void Disconnect()
-//        {
-
-//        }
-
-//        /// <summary>
-//        /// 开始接受消息
-//        /// </summary>
-//        public void BeginReceive()
-//        {
-
-//        }
-
-//        /// <summary>
-//        /// 发送消息
-//        /// </summary>
-//        public void Send()
-//        {
-
-//        }
-//        #endregion//公开方法
-
-//        #region 回调函数
-
-//        private void OnAccept(IAsyncResult iar) { }
-//        private void OnConnect(IAsyncResult iar) { }
-//        private void OnDisconnect(IAsyncResult iar) { }
-//        private void OnSend(IAsyncResult iar) { }
-//        private void OnReceive(IAsyncResult iar) { }
-
-//        #endregion//回调函数
+                //本地地址
+                IPEndPoint local = socket.LocalEndPoint as IPEndPoint;
+                localAddress = local.Address.ToString();
+                localPort = local.Port;
+            }
+        }
 
 
-//    }
+        #region 公开方法用于执行各种Socket行为
+        /// <summary>
+        /// 开始监听等待接收到新信息的回调
+        /// </summary>
+        public bool BeginListen(int port, int queueSize = 1000)
+        {
+            localAddress = "127.0.0.1";
+            localPort = port;
 
-//}
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socket.Bind(new IPEndPoint(IPAddress.Any, localPort));
+                socket.Listen(queueSize);
+                socket.BeginAccept(acceptCallback, null);
+            }
+            catch (SocketException se)
+            {
+                if (onListen != null)
+                    onListen(2, se.ErrorCode, se.Message);
+
+                if (onError != null)
+                    onError(SocketState.BEGIN_LISTEN, se.ErrorCode, se.Message);
+
+                return false;
+            }
+            catch (Exception exp)
+            {
+                if (onListen != null)
+                    onListen(2, 0, exp.Message);
+
+                if (onError != null)
+                    onError(SocketState.BEGIN_LISTEN, 0, exp.Message);
+
+                return false;
+            }
+
+            if (onListen != null)
+                onListen(0, 0, "");
+
+            return true;
+        }
+
+        /// <summary>
+        /// 停止监听
+        /// </summary>
+        public void StopListen()
+        {
+            //设置主动关闭标志位
+            isCloseSelf = true;
+
+            try
+            {
+                //停止监听
+                if (socket != null)
+                    socket.Close();
+            }
+            catch (SocketException se)
+            {
+                //引发报错事件
+                if (onError != null)
+                    onError(SocketState.END_LISTEN, se.ErrorCode, se.Message);
+            }
+            catch (Exception exp)
+            {
+                //引发报错事件
+                if (onError != null)
+                    onError(SocketState.END_LISTEN, 0, exp.Message);
+            }
+        }
+
+        /// <summary>
+        /// 连接等待成功连接的回调
+        /// </summary>
+        public void Connect(string address, int port)
+        {
+            remoteAddress = address;
+            remotePort = port;
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            isCloseSelf = false;
+
+            try
+            {
+                //尝试连接
+                socket.BeginConnect(remoteAddress, remotePort, connectCallback, null);
+            }
+            catch (SocketException se)
+            {
+                if (onConnect != null)
+                    onConnect(1, se.ErrorCode, se.Message);
+
+                if (onError != null)
+                    onError(SocketState.BEGIN_CONNECT, se.ErrorCode, se.Message);
+            }
+            catch(Exception e)
+            {
+                if (onConnect != null)
+                    onConnect(2, 0, e.Message);
+
+                if (onError != null)
+                    onError(SocketState.BEGIN_CONNECT, 0, e.Message);
+            }
+        }
+
+        /// <summary>
+        /// 断开连接等待断开成功的回调
+        /// </summary>
+        public void Disconnect()
+        {
+            //判断是否已连接
+            if (!isConneted)
+                return;
+
+            //设置主动关闭位
+            isCloseSelf = true;
+
+            try
+            {
+                //开始断开连接
+                socket.BeginDisconnect(false, disconnectCallback, null);
+            }
+            catch (SocketException se)
+            {
+                //引发报错事件
+                if (onError != null)
+                    onError(SocketState.BEGIN_DISCONNECT, se.ErrorCode, se.Message);
+            }
+            catch (Exception exp)
+            {
+                //引发报错事件
+                if (onError != null)
+                    onError(SocketState.BEGIN_DISCONNECT, 0, exp.Message);
+            }
+        }
+
+        /// <summary>
+        /// 开始接受消息等待回调
+        /// </summary>
+        public void BeginReceive()
+        {
+            try
+            {
+                //开始接收数据
+                socket.BeginReceive(receiveBuffer, 0, receiveBuffer.Length, 0, receiveCallback, null);
+            }
+            catch (SocketException se)
+            {
+                //引发报错事件
+                if (onError != null)
+                    onError(SocketState.BEGIN_RECEIVE, se.ErrorCode, se.Message);
+            }
+            catch (Exception exp)
+            {
+                //引发报错事件
+                if (onError != null)
+                    onError(SocketState.BEGIN_RECEIVE, 0, exp.Message);
+            }
+        }
+
+        /// <summary>
+        /// 发送消息并等待发送成功的回调
+        /// </summary>
+        public void Send(byte[] data)
+        {
+            try
+            {
+                //开始接收数据
+                socket.BeginSend(data, 0, data.Length, 0, sendCallback, null);
+            }
+            catch (SocketException se)
+            {
+                //引发报错事件
+                if (onError != null)
+                    onError(SocketState.BEGIN_SEND, se.ErrorCode, se.Message);
+            }
+            catch (Exception exp)
+            {
+                //引发报错事件
+                if (onError != null)
+                    onError(SocketState.BEGIN_SEND, 0, exp.Message);
+            }
+        }
+        #endregion//公开方法
+
+        #region 回调函数
+        /// <summary>
+        /// 当接收完成时回调
+        /// </summary>
+        private AsyncCallback acceptCallback;
+        private void OnAccept(IAsyncResult iar)
+        {
+            Socket client;
+
+            try
+            {
+                //获取客户端套接字
+                client = socket.EndAccept(iar);
+                //继续等待连接
+                socket.BeginAccept(acceptCallback, null);
+            }
+            catch(SocketException se)
+            {
+                if (onError != null)
+                    onError(SocketState.ACCEPT, se.ErrorCode, se.Message);
+            }
+        }
+        /// <summary>
+        /// 当连接完成时回调
+        /// </summary>
+        private AsyncCallback connectCallback;
+        private void OnConnect(IAsyncResult iar) { }
+        /// <summary>
+        /// 当断开连接时回调
+        /// </summary>
+        private AsyncCallback disconnectCallback;
+        private void OnDisconnect(IAsyncResult iar) { }
+        /// <summary>
+        /// 当发送完成时回调
+        /// </summary>
+        private AsyncCallback sendCallback;
+        private void OnSend(IAsyncResult iar) { }
+        /// <summary>
+        /// 当接收信息时回调
+        /// </summary>
+        private AsyncCallback receiveCallback;
+        private void OnReceive(IAsyncResult iar) { }
+
+
+        #endregion//回调函数
+
+
+    }
+
+}
