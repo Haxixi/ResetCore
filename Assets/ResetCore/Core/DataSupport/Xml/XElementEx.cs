be6b7bf649fc16f5318508cfcb32b256d6fe9494
董宸 <352070977@qq.com>
2016-12-12 20:39:05 +0800

    更新XmlEx

--- Assets/ResetCore/Core/DataSupport/Xml/XElementEx.cs
+++ Assets/ResetCore/Core/DataSupport/Xml/XElementEx.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Xml.Linq;
 using ResetCore.Util;
 using System.Collections.Generic;
+using System;
 
 namespace ResetCore.Xml
 {
@@ -27,6 +28,50 @@ namespace ResetCore.Xml
         }
 
         /// <summary>
+        /// 从节点中获取数据
+        /// </summary>
+        /// <param name="_el"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static object ReadValueFromElement(this XElement _el, Type type)
+        {
+            object genericArgument;
+            if (type.IsGenericType && (type.GetGenericTypeDefinition() == typeof(List<>)))
+            {
+                System.Type type2 = type.GetGenericArguments()[0];
+                List<XElement> list = new List<XElement>(_el.Elements());
+                object constructor = type.GetConstructor(System.Type.EmptyTypes).Invoke(null);
+                foreach (XElement el in list)
+                {
+                    genericArgument = el.ReadValueFromElement(type2);
+                    type.GetMethod("Add").Invoke(constructor, new object[] { genericArgument });
+                }
+                return constructor;
+            }
+            else if (type.IsGenericType && (type.GetGenericTypeDefinition() == typeof(Dictionary<,>)))
+            {
+                System.Type[] genericArguments = type.GetGenericArguments();
+                Dictionary<string, XElement> dictionary = _el.GetElementDict();
+                var constructor = type.GetConstructor(System.Type.EmptyTypes).Invoke(null);
+                if (!genericArguments[0].Equals(typeof(string)))
+                {
+                    Debug.LogError("键值必须为字符串！");
+                    return null;
+                }
+                foreach (KeyValuePair<string, XElement> pair in dictionary)
+                {
+                    genericArgument = pair.Value.ReadValueFromElement(genericArguments[1]);
+                    type.GetMethod("Add").Invoke(constructor, new object[] { pair.Key, genericArgument });
+                }
+                return constructor;
+            }
+            else
+            {
+                return StringEx.GetValue(_el.Value, type);
+            }
+        }
+
+        /// <summary>
         /// 从节点中读取值
         /// </summary>
         /// <typeparam name="T"></typeparam>
@@ -35,7 +80,8 @@ namespace ResetCore.Xml
         /// <returns></returns>
         public static T ReadValueFromElement<T>(this XElement _el, T _defValue = default(T))
         {
-            return (T)StringEx.GetValue(_el.Value, typeof(T));
+            Type type = typeof(T);
+            return (T)ReadValueFromElement(_el, typeof(T));
         }
 
         /// <summary>
@@ -44,18 +90,45 @@ namespace ResetCore.Xml
         /// <typeparam name="T"></typeparam>
         /// <param name="_root"></param>
         /// <returns></returns>
-        public static Dictionary<string, T> ReadDictionaryFromElement<T>(this XElement _root)
+        public static Dictionary<string, T> ReadDictionaryFromElement<T>(this XElement root)
         {
             Dictionary<string, T> _dictionary = new Dictionary<string, T>();
-            foreach (XElement el in _root.Elements())
+            foreach (XElement el in root.Elements())
             {
                 if (_dictionary.ContainsKey(el.Name.ToString()))
-                    Debug.Log("同一元素在XML中重复定义");
-                _dictionary.Add(el.Name.ToString(), (T)StringEx.GetValue(el.Value, typeof(T)));
+                    Debug.LogError("同一元素在XML中重复定义");
+                _dictionary.Add(el.Name.ToString(), el.ReadValueFromElement<T>());
             }
             return _dictionary;
         }
 
+        private static Dictionary<string, XElement> GetElementDict(this XElement root)
+        {
+            Dictionary<string, XElement> _dictionary = new Dictionary<string, XElement>();
+            foreach (XElement el in root.Elements())
+            {
+                if (_dictionary.ContainsKey(el.Name.ToString()))
+                    Debug.LogError("同一元素在XML中重复定义");
+                _dictionary.Add(el.Name.ToString(), el);
+            }
+            return _dictionary;
+        }
+
+        /// <summary>
+        /// 从节点中读取List
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static List<T> ReadListFromElement<T>(this XElement root)
+        {
+            List<T> list = new List<T>();
+            foreach(XElement el in root.Elements())
+            {
+                list.Add(el.ReadValueFromElement<T>());
+            }
+            return list;
+        }
 
     }
 
