52fe5d750be6c59e5ed426880a17f27ea81e61e1
董宸 <352070977@qq.com>
2017-02-19 19:56:13 +0800

    修复行为队列无法停止的问题（Lambda表达式的陷阱）

--- Assets/ResetCore/Core/Util/ActionQueue.cs
+++ Assets/ResetCore/Core/Util/ActionQueue.cs
@@ -34,7 +34,7 @@ namespace ResetCore.Util
                 //队列非空，则加入队列
                 this.m_queue.Enqueue(() =>
                 {
-                    this.WaitAndDo(actionCB, this.m_queue);
+                    this.WaitAndDo(actionCB);
                 });
             }
             else
@@ -42,28 +42,29 @@ namespace ResetCore.Util
                 //现在正在进行调用isLoading为true
                 isLoading = true;
                 //队列为空，直接调用，但是下一个行为在运行时需要加入队列
-                this.WaitAndDo(actionCB, this.m_queue);
+                this.WaitAndDo(actionCB);
             }
             return this;
         }
 
-        private void WaitAndDo(Action<Action> actionCB, Queue<Action> q)
+        private void WaitAndDo(Action<Action> actionCB)
         {
-            Action callBack = () =>
-            {
-                if (q.Count != 0)
-                {
-                    //出队列得到Action并且调用
-                    q.Dequeue().Invoke();
-                }
-                else
-                {
-                    //表示队列为空，下次可以直接进行调用
-                    isLoading = false;
-                }
-            };
             //在行为调用完成之后进行回调
-            actionCB(callBack);
+            actionCB(TryDequeue);
+        }
+
+        private void TryDequeue()
+        {
+            if (m_queue.Count != 0)
+            {
+                //出队列得到Action并且调用
+                m_queue.Dequeue().Invoke();
+            }
+            else
+            {
+                //表示队列为空，下次可以直接进行调用
+                isLoading = false;
+            }
         }
 
         /// <summary>
@@ -93,22 +94,9 @@ namespace ResetCore.Util
 
         private void WaitAndDo(Action actionCB, Queue<Action> q)
         {
-            Action callBack = () =>
-            {
-                if (q.Count != 0)
-                {
-                    //出队列得到Action并且调用
-                    q.Dequeue().Invoke();
-                }
-                else
-                {
-                    //表示队列为空，下次可以直接进行调用
-                    isLoading = false;
-                }
-            };
             //在行为调用完成之后进行回调
             actionCB();
-            callBack();
+            TryDequeue();
         }
 
         /// <summary>
@@ -123,7 +111,7 @@ namespace ResetCore.Util
                 Action<bool> callBack = (bo) => { act(); };
                 CoroutineTaskManager.Instance.AddTask(actionCB, callBack);
             });
-            
+
         }
 
         #endregion
--- Assets/ResetCore/Test/Editor/TestBton.cs
+++ Assets/ResetCore/Test/Editor/TestBton.cs
@@ -2,13 +2,42 @@
 using UnityEditor;
 using ResetCore.PlatformHelper;
 using System.Collections.Generic;
+using System;
+using System.Diagnostics;
 
 public class TestBton {
 
 	[MenuItem("Test/Test")]
 	public static void EditorTest()
 	{
-        //AndroidPluginBuilder.BuildMenifest(@"F:\UnityProjects\ResetCore\ResetCoreTemp\SDK\GeTui\build.xml", new List<string>());
+        Action<Action> testTime = (act) =>
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            act();
+            watch.Stop();
+            UnityEngine.Debug.Log(watch.ElapsedTicks);
+            watch.Reset();
+        };
+
+        Dictionary<int, int> testDict = new Dictionary<int, int>();
+        Dictionary<string, int> testDict2 = new Dictionary<string, int>();
+
+        for (int i = 0; i < 100000; i++)
+        {
+            testDict.Add(i, i + 1);
+            testDict2.Add(i.ToString(), i + 1);
+        }
+
+        testTime(() =>
+        {
+            var item = testDict2["5000"];
+        });
+
+        //testTime(() =>
+        //{
+        //    var item = testDict2["50"];
+        //});
 
     }
 }
