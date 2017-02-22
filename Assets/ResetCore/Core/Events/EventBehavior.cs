1c8feff4cce93a245653b34af45dd13684abedbc
dc <dc@wooduan.com>
2017-02-22 22:57:51 +0800

    进一步更新绑定事件的范围，扩展到值域与属性。

--- Assets/ResetCore/Core/Events/EventBehavior.cs
+++ Assets/ResetCore/Core/Events/EventBehavior.cs
@@ -9,30 +9,64 @@ namespace ResetCore.Event
     /// <summary>
     /// 代表了能够生成事件的函数
     /// </summary>
-    [AttributeUsage(AttributeTargets.Method)]
-    public class GenEventable : Attribute
+    [AttributeUsage(AttributeTargets.Method|AttributeTargets.Field|AttributeTargets.Property)]
+    public sealed class GenEventable : Attribute
     {
         public string[] eventName { get; private set; }
-        public GenEventable(string[] eventName)
+
+        public GenEventable(params string[] eventName)
         {
             this.eventName = eventName;
         }
     }
-    public class EventBehavior
+
+    public sealed static class EventBehavior
     {
+
+        /// <summary>
+        /// 为所有绑定了GenEventable的函数、属性、值域绑定事件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="mono"></param>
         public static void GenEvent<T>(T mono) where T:MonoBehaviour
         {
             Type monoType = mono.GetType();
 
+            //绑定函数
             MethodInfo[] methodInfos = monoType.GetMethods();
             HandleMethods<T>(mono, methodInfos);
+
+            //绑定属性
+            PropertyInfo[] propInfos = monoType.GetProperties();
+            HandleProperties(mono, propInfos);
+
+            //绑定值域
+            FieldInfo[] fieldInfos = monoType.GetFields();
+            HandleFields(mono, fieldInfos);
         }
 
+        /// <summary>
+        /// 清除事件
+        /// </summary>
+        /// <param name="mono"></param>
         public static void ClearEvent(MonoBehaviour mono)
         {
             MonoEventDispatcher.monoEventControllerDict.Remove(mono);
         }
 
+        /// <summary>
+        /// 绑定数据
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="mono"></param>
+        /// <param name="eventName"></param>
+        /// <param name="setter"></param>
+        public static void BindData<T>(this MonoBehaviour mono, string eventName, Action<T> setter)
+        {
+            EventDispatcher.AddEventListener<T>(eventName, setter, mono);
+        }
+
+        #region 私有函数
         private static void HandleMethods<T>(T mono, MethodInfo[] methodInfos)
         {
             for(int i = 0; i < methodInfos.Length; i++)
@@ -98,5 +132,64 @@ namespace ResetCore.Event
                 }
             }
         }
+
+        private static void HandleProperties<T>(T mono, PropertyInfo[] propertyInfos)
+        {
+            for (int i = 0; i < propertyInfos.Length; i++)
+            {
+                HandleProperty(mono, propertyInfos[i]);
+            }
+        }
+
+        private static void HandleProperty<T>(T mono, PropertyInfo propertyInfo)
+        {
+            object[] attrs = propertyInfo.GetCustomAttributes(typeof(GenEventable), true);
+            if (attrs.Length == 0) return;
+            GenEventable genEventAttr = attrs[0] as GenEventable;
+
+            var eventNames = genEventAttr.eventName;
+
+            if (eventNames == null || eventNames.Length == 0)
+            {
+                eventNames = new string[] { propertyInfo.Name };
+            }
+
+            for (int i = 0; i < eventNames.Length; i++)
+            {
+                EventDispatcher.AddEventListener<object>(eventNames[i],
+                                (arg1) => { propertyInfo.SetValue(mono, arg1, new object[0]); }, mono);
+            }
+                
+        }
+
+        private static void HandleFields<T>(T mono, FieldInfo[] fieldInfos)
+        {
+            for (int i = 0; i < fieldInfos.Length; i++)
+            {
+                HandleField(mono, fieldInfos[i]);
+            }
+        }
+
+        private static void HandleField<T>(T mono, FieldInfo fieldInfo)
+        {
+            object[] attrs = fieldInfo.GetCustomAttributes(typeof(GenEventable), true);
+            if (attrs.Length == 0) return;
+            GenEventable genEventAttr = attrs[0] as GenEventable;
+
+            var eventNames = genEventAttr.eventName;
+
+            if (eventNames == null || eventNames.Length == 0)
+            {
+                eventNames = new string[] { fieldInfo.Name };
+            }
+
+            for (int i = 0; i < eventNames.Length; i++)
+            {
+                EventDispatcher.AddEventListener<object>(eventNames[i],
+                                (arg1) => { fieldInfo.SetValue(mono, arg1); }, mono);
+            }
+        }
+        #endregion 私有函数
+
     }
 }
