dace98d106c782ec8bd53b591ffb9be09085c7ed
董宸 <352070977@qq.com>
2017-03-18 00:35:01 +0800

    添加序列化与反序列化列表

--- Assets/ResetCore/Core/DataSupport/Xml/XDocumentEx.cs
+++ Assets/ResetCore/Core/DataSupport/Xml/XDocumentEx.cs
@@ -160,15 +160,7 @@ namespace ResetCore.Xml
             return xDoc;
         }
 
-        /// <summary>
-        /// 合并Xml
-        /// </summary>
-        /// <param name="baseDoc"></param>
-        /// <param name="add"></param>
-        public static void Merge(this XDocument baseDoc, XDocument add)
-        {
-
-        }
+       
     }
 
 }
