1be2b8e70ea949a3822bacf31e333ddaf760557c
dc <dc@wooduan.com>
2017-04-12 22:56:55 +0800

    更新一些测试代码

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -14,7 +14,22 @@ public class Driver : MonoSingleton<Driver> {
 
     void Awake()
     {
-        //Debug.Log(BuffData.dataMap[1].BuffName);
+        EventDispatcher.AddEventListener<int, int, int, int>("Test", Test);
+        EventDispatcher.TriggerEvent("Test", 1, 2, 3, 4);
+        CoroutineTaskManager.Instance.WaitSecondTodo(() =>
+        {
+            EventDispatcher.RemoveEventListener<int, int, int, int>("Test", Test);
+            EventDispatcher.TriggerEvent("Test", 1, 2, 3, 4);
+            Debug.Log("1111111111111111111");
+        }, 2);
+    }
+
+    public void Test(int i1, int i2, int i3, int i4)
+    {
+        Debug.Log(i1);
+        Debug.Log(i2);
+        Debug.Log(i3);
+        Debug.Log(i4);
     }
 
     // Use this for initialization
