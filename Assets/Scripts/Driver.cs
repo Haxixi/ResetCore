69601090354e35d0d4788432251dc9c5aeefd7d2
董宸 <352070977@qq.com>
2017-05-05 14:02:14 +0800

    gx

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -11,18 +11,49 @@ public class Driver : MonoSingleton<Driver> {
 
     void Awake()
     {
+        ReCoroutineTaskManager.Instance.WaitForAllCoroutine(() =>
+        {
+            Debug.Log("FinishAll");
+        }, new ReCoroutineTaskManager.CoroutineTask[] {
+            EventDispatcher.AddEventListener("Test", Test).GetListenLatestCommandCoroutine(),
+            EventDispatcher.AddEventListener("Test2", Test2).GetListenLatestCommandCoroutine(),
+            EventDispatcher.AddEventListener("Test3", Test3).GetListenLatestCommandCoroutine(),
+            EventDispatcher.AddEventListener("Test4", Test4).GetListenLatestCommandCoroutine()
+        });
+
         ReCoroutineManager.AddCoroutine(TestCount());
     }
 
     IEnumerator<float> TestCount()
     {
-        int sum = 0;
-        yield return ReCoroutine.WaitThreadOperation(() =>
-        {
-            for (int i = 0; i < 100000; i++)
-                sum++;
-        });
-        Debug.Log(sum);
+        yield return 1;
+        EventDispatcher.TriggerEvent("Test");
+        yield return 1;
+        EventDispatcher.TriggerEvent("Test2");
+        yield return 1;
+        EventDispatcher.TriggerEvent("Test3");
+        yield return 1;
+        EventDispatcher.TriggerEvent("Test4");
+    }
+
+    private void Test()
+    {
+        Debug.Log("Finish Test1");
+    }
+
+    private void Test2()
+    {
+        Debug.Log("Finish Test2");
+    }
+
+    private void Test3()
+    {
+        Debug.Log("Finish Test3");
+    }
+
+    private void Test4()
+    {
+        Debug.Log("Finish Test4");
     }
 
     void Update()
